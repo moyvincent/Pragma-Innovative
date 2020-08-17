b4efe5999249f661b82ebeb8e999255af047aa9f
Vincent Moyaki <moyakivincent@yahoo.com>
2020-08-17 01:55:57 +0100

    Modified Sound of Africa Admin page

--- Pradma/Pradma/settings.py
+++ Pradma/Pradma/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -64,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -120,5 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
+MEDIA_URL= "/media/"
 
 AUTH_USER_MODEL = 'users.CustomUser'
--- Pradma/sound_of_africa/admin.py
+++ Pradma/sound_of_africa/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Sound_Audio, Sound_Video
 
 # Register your models here.
+class PradmaVideoAdmin(admin.ModelAdmin):
+    list_display = ['video_upload_by', 'video_title','video_upload', 'video_timestamp',]
+
+class PradmaAudioAdmin(admin.ModelAdmin):
+    list_display = ['audio_upload_by', 'audio_title','audio_upload', 'audio_timestamp',]
+
+admin.site.register(Sound_Video, PradmaVideoAdmin)
+admin.site.register(Sound_Audio, PradmaAudioAdmin)
